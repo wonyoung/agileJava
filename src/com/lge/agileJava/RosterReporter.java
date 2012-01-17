c57758c5bf2c98352e557c4bcf5ec31335a1d08b
Wonyoung Jang <wonyoungjjang@gmail.com>
2012-01-17 11:54:43 +0900

    Lesson 3. - Refactoring

--- src/com/lge/agileJava/RosterReporter.java
+++ src/com/lge/agileJava/RosterReporter.java
@@ -13,16 +13,24 @@ public class RosterReporter {
 	
 	String getReport() {
 		StringBuilder buffer = new StringBuilder();
-		
+		writeHeader(buffer);
+		writeBody(buffer);
+		writeFooter(buffer);
+
+		return buffer.toString();
+	}
+
+	void writeHeader(StringBuilder buffer) {
 		buffer.append(ROSTER_REPORT_HEADER);
-		
+	}	
+	void writeBody(StringBuilder buffer) {
 		for (Student student : session.getAllStudents()) {
 			buffer.append(student.getName());
 			buffer.append(NEWLINE);
 		}
-				
+	}
+	void writeFooter(StringBuilder buffer) {
 		buffer.append(ROSTER_REPORT_FOOTER + session.getAllStudents().size() + NEWLINE);
-		
-		return buffer.toString();
-	}	
+	}
 }
+
