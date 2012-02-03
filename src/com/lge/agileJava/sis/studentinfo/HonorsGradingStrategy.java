759449c022e42e73e0c83aaf102b5faf4b41fc39
Wonyoung Jang <wonyoungjjang@gmail.com>
2012-02-03 11:16:05 +0900

    Lesson 6. Inheritance -Case Labels are Just Labels

--- src/com/lge/agileJava/sis/studentinfo/HonorsGradingStrategy.java
+++ src/com/lge/agileJava/sis/studentinfo/HonorsGradingStrategy.java
@@ -12,10 +12,12 @@ public class HonorsGradingStrategy implements GradingStrategy {
 	}
 
 	int basicGradePointsFor(Grade grade) {
-		if (grade == Grade.A) return 4;
-		if (grade == Grade.B) return 3;
-		if (grade == Grade.C) return 2;
-		if (grade == Grade.D) return 1;
-		return 0;
+		switch (grade) {
+			case A: return 4;
+			case B: return 3;
+			case C: return 2;
+			case D: return 1;
+			default : return 0;
+		}
 	}
 }
--- src/com/lge/agileJava/sis/studentinfo/RegularGradingStrategy.java
+++ src/com/lge/agileJava/sis/studentinfo/RegularGradingStrategy.java
@@ -6,11 +6,13 @@ public class RegularGradingStrategy implements GradingStrategy {
 
 	@Override
 	public int getGradePointsFor(Grade grade) {
-		if (grade == Grade.A) return 4;
-		if (grade == Grade.B) return 3;
-		if (grade == Grade.C) return 2;
-		if (grade == Grade.D) return 1;
-		return 0;
+		switch (grade) {
+			case A: return 4;
+			case B: return 3;
+			case C: return 2;
+			case D: return 1;
+			default : return 0;
+		}
 	}
 
 }
